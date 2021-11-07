d758e6bf034e7bf7ff28ad9cbf110d6988fdb797
DR DJAHA <paxeldp@gmail.com>
2021-11-07 01:17:58 +0000

    initial commit

--- public/js/confirm.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    window.confirmAction = function (input){
-        var url = $(input).closest("td").attr("data-url");
-        $("#statutForm").prop("action", url);
-    }
-});
